66ef1c9ce91315ce3f9c6284202496d959694216
Andr-he <andre.h@sempreceub.com>
2026-05-16 16:12:43 -0300

    Create 1012.py

---
+++ arquivos/1012.py
@@ -0,0 +1,14 @@
+A, B, C = map(float, input().split())
+pi= 3.14159
+
+Triangulo = A*C/2
+Circulo = pi*C**2
+Trapezio = (A+B)*C/2
+Quadrado = B**2
+Retangulo = A*B
+
+print(f'TRIANGULO: {Triangulo:.3f}')
+print(f'CIRCULO: {Circulo:.3f}')
+print(f'TRAPEZIO: {Trapezio:.3f}')
+print(f'QUADRADO: {Quadrado:.3f}')
+print(f'RETANGULO: {Retangulo:.3f}')
