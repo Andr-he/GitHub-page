bdb5827d0eadff6e37896b8e78cd7c5266e93444
Andr-he <andre.h@sempreceub.com>
2026-05-16 16:12:54 -0300

    Create 1008.py

---
+++ arquivos/1008.py
@@ -0,0 +1,8 @@
+NumeroFuncionarios=int(input())
+HorasTrabalhadas=int(input())
+ValorHora=float(input())
+
+Salario=HorasTrabalhadas*ValorHora
+
+print(f'NUMBER = {NumeroFuncionarios}')
+print(f'SALARY = U${Salario:.2f}')
