5bc8e9ebff594913e3c4eb2be4ede6e8d2eb287c
Andr-he <andre.h@sempreceub.com>
2026-05-16 16:12:08 -0300

    Create 1037.py

---
+++ arquivos/1037.py
@@ -0,0 +1,9 @@
+N = float(input())
+if N <= 25.00:
+    print('Intervalo [0,25]')
+elif N <= 25.50:
+    print('Intervalo (25,50]')
+elif N <= 50.75:
+    print('Intervalo (50,75]')
+elif N <= 75.100:
+    print('Intervalo (75,100]')
